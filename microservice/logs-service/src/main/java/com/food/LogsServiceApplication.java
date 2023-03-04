a9e3c294d56d45473e937a447c4b2f03a433fb7f
Savaş Dede <svsdd22@gmail.com>
2023-03-04 14:06:09 +0300

    eureka client add all service

--- microservice/logs-service/src/main/java/com/food/LogsServiceApplication.java
+++ microservice/logs-service/src/main/java/com/food/LogsServiceApplication.java
@@ -2,8 +2,10 @@ package com.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LogsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LogsServiceApplication.class,args);
