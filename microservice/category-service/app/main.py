477a5ff36294e80259883b7741a82893a01c637e
Savaş Dede <svsdd22@gmail.com>
2023-03-04 20:56:06 +0300

    gateway

--- microservice/category-service/app/main.py
+++ microservice/category-service/app/main.py
@@ -2,21 +2,9 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
 from app.controller import personel_api
-#import py_eureka_client.eureka_client as eureka_client
-
-# port = 8082
-# client = eureka_client.init(
-#     eureka_server="http://127.0.0.1:8761/eureka",
-#     #eureka_protocol="http",
-#     #eureka_context="/eureka/v2",
-#     app_name="category-service",
-#     #instance_ip="192.168.1.21/
-#     # 24",
-#     instance_port=port)
 
 
 app = FastAPI()
-
 
 
 origins = [settings.CLIENT_ORIGIN]
@@ -27,7 +15,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
 @app.get('/api/health')
