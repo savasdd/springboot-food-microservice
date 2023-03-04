286008cf5b472404e28d87e4f7fb9f267daa4233
Savaş Dede <svsdd22@gmail.com>
2023-03-04 16:32:58 +0300

    food

--- microservice/category-service/app/main.py
+++ microservice/category-service/app/main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
 from app.controller import personel_api
+#import py_eureka_client.eureka_client as eureka_client
+from py_eureka_client.eureka_client import EurekaClient
 
+port = 8082
+client=EurekaClient(eureka_server="http://127.0.0.1:8761/eureka",
+                   app_name="category-service", instance_port=port)
 
 app = FastAPI()
+
+
 
 origins = [settings.CLIENT_ORIGIN]
 app.add_middleware(
@@ -15,7 +22,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+client.start()
+
 @app.get('/api/health')
 def root():
     return {'response': 'Category Service FastApi'}
-
