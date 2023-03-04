bff2945bdd5cf93dc69eff2fd15de718b86dc404
Savaş Dede <svsdd22@gmail.com>
2023-03-04 13:35:58 +0300

    logs service

---
+++ microservice/logs-service/src/main/java/com/food/LogsServiceApplication.java
@@ -0,0 +1,11 @@
+package com.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogsServiceApplication.class,args);
+    }
+}
