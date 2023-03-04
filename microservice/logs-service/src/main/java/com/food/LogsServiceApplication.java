4f8995d794d5657e741bac9f9c0b1167b1ea0f19
Savaş Dede <svsdd22@gmail.com>
2023-03-04 14:35:48 +0300

    catory

--- microservice/logs-service/src/main/java/com/food/LogsServiceApplication.java
+++ microservice/logs-service/src/main/java/com/food/LogsServiceApplication.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class LogsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LogsServiceApplication.class,args);
+        System.out.println("test");
     }
 }
