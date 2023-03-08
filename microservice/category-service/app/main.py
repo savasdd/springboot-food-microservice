c93a5e81052e2c4bee0c4b07b989ffdf541c835b
Savaş Dede <svsdd22@gmail.com>
2023-03-08 20:33:48 +0300

    db

--- microservice/category-service/app/main.py
+++ microservice/category-service/app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
 from app.controller import personel_api
 from kafka import KafkaProducer
+import json
 
 
 app = FastAPI()
@@ -18,9 +19,14 @@ app.add_middleware(
 )
 
 
-@app.get('/api/categoriys/send')
+@app.get('/api/categorys/send')
 def root():
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-    producer.send('FOOD', b'Elma Kategorisi: 250')
+    AaccountEvent = {
+        "name": "Elma Kategorisi: 250",
+    }
+
+    producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    producer.send('FOOD', AaccountEvent)
     producer.flush()
     return {'response': 'Category Service FastApi'}
