3757a9343100f7bd42695726de308f80a89bd6b4
savas.dede <savas.dede@enerji.gov.tr>
2023-03-09 16:53:22 +0300

    keycloak entegrations

---
+++ microservice/api-gateway/src/main/java/com/food/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.food.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        configuration.addAllowedHeader("*");
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        http.csrf().disable()
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .pathMatchers("/eureka/**").permitAll()
+                        .pathMatchers("/api/auth").permitAll()
+                        .pathMatchers("/api/resfresh").permitAll()
+                        .pathMatchers( "/actuator/**").permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return http.build();
+    }
+
+}
--- microservice/api-gateway/src/main/java/com/food/utils/GatewayUtils.java
+++ microservice/api-gateway/src/main/java/com/food/utils/GatewayUtils.java
@@ -6,7 +6,7 @@ import lombok.experimental.UtilityClass;
 public class GatewayUtils {
 
     //Keycloak
-    public static final String KEYC_GROUP = "FOOD_SORGULAMA";
+    public static final String KEYC_GROUP = "FOOD_SEARCH";
     public static final String ADMIN_ROLU = "ADMIN_ROLU";
     public static final String USER_ROLU = "USER_ROLU";
 
