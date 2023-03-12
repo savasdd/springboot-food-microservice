db8ed35b333773a4a2509cdf124096b222a9de89
Savaş Dede <svsdd22@gmail.com>
2023-03-12 15:58:32 +0300

    account mysql db

--- microservice/account-service/src/main/java/com/food/model/Account.java
+++ microservice/account-service/src/main/java/com/food/model/Account.java
@@ -5,9 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -39,5 +43,17 @@ public class Account {
     @Version
     @Column(name = "VERSION")
     private Long version;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private String updatedBy;
+
+    @CreatedDate
+    private Date createdDate;
+
+    @LastModifiedDate
+    private Date updatedDate;
 
 }
