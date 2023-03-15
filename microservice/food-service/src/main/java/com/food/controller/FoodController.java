c1ac91ab3681a4d088c73abb0a99e3e5b9e4517b
Savaş Dede <svsdd22@gmail.com>
2023-03-15 20:54:12 +0300

    log aop

--- microservice/food-service/src/main/java/com/food/controller/FoodController.java
+++ microservice/food-service/src/main/java/com/food/controller/FoodController.java
@@ -2,6 +2,7 @@ package com.food.controller;
 
 import com.food.dto.FoodDto;
 import com.food.service.FoodService;
+import com.food.utils.aop.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,21 +25,25 @@ public class FoodController {
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
+
     @GetMapping(value = "/foods")
     public ResponseEntity<List<FoodDto>> getAll(){
         return new ResponseEntity<>(service.getFoodService().getAll(), HttpStatus.OK);
     }
 
+    @Log
     @PostMapping(value = "/foods")
     public ResponseEntity<FoodDto> create(@RequestBody FoodDto dto){
         return new ResponseEntity<>(service.getFoodService().create(dto),HttpStatus.CREATED);
     }
 
+    @Log
     @PutMapping(value = "/foods/{id}")
     public ResponseEntity<FoodDto> update(@PathVariable("id") UUID id,@RequestBody FoodDto dto){
         return new ResponseEntity<>(service.getFoodService().update(id, dto),HttpStatus.OK);
     }
 
+    @Log
     @DeleteMapping(value = "/foods/{id}")
     public ResponseEntity<FoodDto> delete(@PathVariable("id") UUID id){
         return new ResponseEntity<>(service.getFoodService().delete(id),HttpStatus.OK);
---
+++ microservice/food-service/src/main/java/com/food/utils/aop/LogAspect.java
@@ -0,0 +1,71 @@
+package com.food.utils.aop;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.ObjectUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    @Autowired
+    ObjectMapper mapper;
+    @Pointcut("@annotation(com.food.utils.aop.Log)")
+    public void logAnnotation() {
+
+    }
+
+    @Around(value = "logAnnotation() && @annotation(log)")
+    public Object createIslemLog(ProceedingJoinPoint joinPoint, Log log) throws Throwable {
+        Object result = null;
+
+        Class<? extends Object> sinif = joinPoint.getTarget().getClass();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method metot = signature.getMethod();
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getRequest();
+
+//        dto.setServis(sinif.getName() + " => " + metot.getName());
+//        dto.setMetot(HttpUtil.getMethod(request));
+//        dto.setPath(HttpUtil.getPath(request));
+//        dto.setCreateDate(new Date());
+//        dto.setKullaniciAdi(util.getUser().getKullaniciAdi());
+        // log.info(HttpUtil.getHeaderMap(request, false));
+
+        ///// Before Method Execution /////
+        result = joinPoint.proceed();
+        ///// After Method Execution /////
+
+        ResponseEntity<?> response = (ResponseEntity<?>) result;
+        if (ObjectUtils.isNotEmpty(response)) {
+            Object body = response.getBody();
+            var status=(long) response.getStatusCodeValue();
+            var json=convertObjectToJson(body);
+            System.out.println(status);
+            System.out.println(json);
+        }
+
+        return result;
+    }
+
+    public String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (ObjectUtils.isEmpty(object)) {
+            return null;
+        }
+        return mapper.writeValueAsString(object);
+    }
+}
