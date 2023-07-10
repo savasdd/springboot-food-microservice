0c276e9acc3f3733750d06c234c4e08f836f3820
Savaş Dede <svsdd22@gmail.com>
2023-07-10 22:33:39 +0300

    reload db

--- microservice/category-service/src/main/java/com/food/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.food.repository;
-
-import com.food.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CategoryRepository extends JpaRepository<Category, UUID> {
-}
