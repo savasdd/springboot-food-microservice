b0a89af85adb715c38699e2c0623e7a18f39e1fe
Savaş Dede <svsdd22@gmail.com>
2023-07-17 15:44:15 +0300

    configure kafka stream to orders

--- microservice/food-service/src/main/java/com/food/FoodServiceApplication.java
+++ microservice/food-service/src/main/java/com/food/FoodServiceApplication.java
@@ -1,13 +1,87 @@
 package com.food;
 
+import com.food.dto.Order;
+import com.food.service.impl.OrderServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueBytesStoreSupplier;
+import org.apache.kafka.streams.state.Stores;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.support.serializer.JsonSerde;
 
+import java.time.Duration;
+
+@Slf4j
 @SpringBootApplication
 @EnableEurekaClient
+@EnableKafkaStreams
 public class FoodServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FoodServiceApplication.class,args);
+        SpringApplication.run(FoodServiceApplication.class, args);
     }
+
+    @Bean
+    public NewTopic orders() {
+        return TopicBuilder.name("orders")
+                .partitions(3)
+                .compact()
+                .build();
+    }
+
+    @Bean
+    public NewTopic paymentTopic() {
+        return TopicBuilder.name("payment-orders")
+                .partitions(3)
+                .compact()
+                .build();
+    }
+
+    @Bean
+    public NewTopic stockTopic() {
+        return TopicBuilder.name("stock-orders")
+                .partitions(3)
+                .compact()
+                .build();
+    }
+
+    @Autowired
+    private OrderServiceImpl orderService;
+
+    @Bean
+    public KStream<Long, Order> stream(StreamsBuilder builder) {
+        JsonSerde<Order> orderSerde = new JsonSerde<>(Order.class);
+        KStream<Long, Order> stream = builder
+                .stream("payment-orders", Consumed.with(Serdes.Long(), orderSerde));
+
+        stream.join(
+                        builder.stream("stock-orders"),
+                        orderService::confirm,
+                        JoinWindows.of(Duration.ofSeconds(10)),
+                        StreamJoined.with(Serdes.Long(), orderSerde, orderSerde))
+                .peek((k, o) -> log.info("Output: {}", o))
+                .to("orders");
+
+        return stream;
+    }
+
+    @Bean
+    public KTable<Long, Order> table(StreamsBuilder builder) {
+        KeyValueBytesStoreSupplier store = Stores.persistentKeyValueStore("orders");
+        JsonSerde<Order> orderSerde = new JsonSerde<>(Order.class);
+        KStream<Long, Order> stream = builder
+                .stream("orders", Consumed.with(Serdes.Long(), orderSerde));
+        return stream.toTable(Materialized.<Long, Order>as(store)
+                .withKeySerde(Serdes.Long())
+                .withValueSerde(orderSerde));
+    }
+
 }
---
+++ microservice/food-service/src/main/java/com/food/service/impl/OrderServiceImpl.java
@@ -0,0 +1,68 @@
+package com.food.service.impl;
+
+import com.food.dto.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+@Service
+public class OrderServiceImpl {
+
+    private AtomicLong id = new AtomicLong();
+    private final KafkaTemplate<Long, Order> template;
+    private final StreamsBuilderFactoryBean kafkaStreamsFactory;
+
+    public OrderServiceImpl(KafkaTemplate<Long, Order> template, StreamsBuilderFactoryBean kafkaStreamsFactory) {
+        this.template = template;
+        this.kafkaStreamsFactory = kafkaStreamsFactory;
+    }
+
+    public Order create(Order order) {
+        order.setId(id.incrementAndGet());
+        template.send("orders", order.getId(), order);
+        log.info("Sent: {}", order);
+        return order;
+    }
+
+    public List<Order> all() {
+        List<Order> orders = new ArrayList<>();
+        ReadOnlyKeyValueStore<Long, Order> store = kafkaStreamsFactory
+                .getKafkaStreams()
+                .store(StoreQueryParameters.fromNameAndType("orders",
+                        QueryableStoreTypes.keyValueStore()));
+        KeyValueIterator<Long, Order> it = store.all();
+        it.forEachRemaining(kv -> orders.add(kv.value));
+        return orders;
+    }
+
+    public Order confirm(Order orderPayment, Order orderStock) {
+        Order o = new Order(orderPayment.getId(),
+                orderPayment.getCustomerId(),
+                orderPayment.getFoodId(),
+                orderPayment.getFoodCount(),
+                orderPayment.getPrice());
+        if (orderPayment.getStatus().equals("ACCEPT") &&
+                orderStock.getStatus().equals("ACCEPT")) {
+            o.setStatus("CONFIRMED");
+        } else if (orderPayment.getStatus().equals("REJECT") &&
+                orderStock.getStatus().equals("REJECT")) {
+            o.setStatus("REJECTED");
+        } else if (orderPayment.getStatus().equals("REJECT") ||
+                orderStock.getStatus().equals("REJECT")) {
+            String source = orderPayment.getStatus().equals("REJECT") ? "PAYMENT" : "STOCK";
+            o.setStatus("ROLLBACK");
+            o.setSource(source);
+        }
+        return o;
+    }
+}
