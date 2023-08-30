adfb82fe374a20c3bc15179ff9692eb98b1ab146
Savaş Dede <svsdd22@gmail.com>
2023-08-30 21:23:04 +0300

    stock fix

--- frontend/src/app/views/foods/stock/stock.component.ts
+++ frontend/src/app/views/foods/stock/stock.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {StockService} from "../../../services/stock.service";
+import {DxDataGridComponent} from "devextreme-angular";
+import {Stock} from "../../../services/stock-service-api";
+import CustomStore from "devextreme/data/custom_store";
+import StatusEnum = Stock.StatusEnum;
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss']
 })
-export class StockComponent {
+export class StockComponent implements OnInit {
+  dataSource: any = {};
+  @ViewChild('stockDataGrid', {static: true}) stockDataGrid: DxDataGridComponent | undefined;
+  events: Array<string> = [];
+  dataTypeSource: any = [
+    {name: StatusEnum.New},
+    {name: StatusEnum.Accept},
+    {name: StatusEnum.Reject},
+    {name: StatusEnum.Confirmed},
+    {name: StatusEnum.Rollback},
+  ];
+
+  constructor(private service: StockService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  loadGrid() {
+    this.dataSource = new CustomStore({
+      key: 'id',
+      load: (loadOptions) => {
+        return this.service.findAll(loadOptions).toPromise().then((response: any) => {
+          return {
+            data: response.data,
+            totalCount: response.totalCount
+          };
+        });
+      },
+
+      byKey: (key) => {
+        return this.service.findOne(key).toPromise().then((response) => {
+          return response;
+        });
+      },
+
+      insert: (values) => {
+        return this.service.save(values).toPromise().then((response) => {
+            return;
+          },
+          err => {
+            throw (err.error.errorMessage ? err.error.errorMessage : err.error.warningMessage);
+          }
+        );
+      },
+      update: (key, values: any) => {
+        values.id = key;
+        return this.service.update(key, values).toPromise().then((response) => {
+            return;
+          },
+          err => {
+            const message = 'Kayıt Güncelleme Hatası: ' + err.error.errorMessage;
+            console.log(message);
+          }
+        );
+      },
+      remove: (key) => {
+        return this.service.delete(key).toPromise().then((response) => {
+            return;
+          },
+          err => {
+            const message = 'Kayıt Silme Hatası: ' + err.error.errorMessage;
+            console.log(message);
+          }
+        );
+      }
+    });
+  }
 
 }
