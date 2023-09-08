f954f4f3e54ef3e1e52bef84cf57238a509c7ac8
Savaş Dede <svsdd22@gmail.com>
2023-09-08 14:35:37 +0300

    user fix

--- frontend/src/app/views/users/user-profile/user-profile.component.ts
+++ frontend/src/app/views/users/user-profile/user-profile.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  @ViewChild('form') form: any = NgForm;
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  updateClick() {
+    console.log("images")
+  }
+
 
 }
