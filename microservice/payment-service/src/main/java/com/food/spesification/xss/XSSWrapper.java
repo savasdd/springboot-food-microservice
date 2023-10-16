d3e5f7b59078ac3b63e5d1c87af1f4c85a3d1b0f
Savaş Dede <svsdd22@gmail.com>
2023-10-16 11:50:29 +0300

    motification

---
+++ microservice/payment-service/src/main/java/com/food/spesification/xss/XSSWrapper.java
@@ -0,0 +1,40 @@
+package com.food.spesification.xss;
+
+import java.util.regex.Pattern;
+
+public class XSSWrapper {
+    private static Pattern[] patterns = new Pattern[]{
+            // Script fragments
+            Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE),
+            // src='...'
+            Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            // lonely script tags
+            Pattern.compile("</script>", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            // eval(...)
+            Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            // expression(...)
+            Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            // javascript:...
+            Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
+            // vbscript:...
+            Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE),
+            // onload(...)=...
+            Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL)
+    };
+
+    public static String stripXSS(String value) {
+        if (value != null) {
+
+            // Avoid null characters
+            value = value.replaceAll("", "");
+
+            // Remove all sections that match a pattern
+            for (Pattern scriptPattern : patterns) {
+                value = scriptPattern.matcher(value).replaceAll("");
+            }
+        }
+        return value;
+    }
+}
---
+++ microservice/stock-service/src/main/java/com/food/spesification/enums/FilterCondition.java
@@ -0,0 +1,5 @@
+package com.food.spesification.enums;
+
+public enum FilterCondition {
+    and, or
+}
