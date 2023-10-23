3228df21f84d3f1d90624ad6fb4bbfef7ce32fc7
Savaş Dede <svsdd22@gmail.com>
2023-10-23 14:43:32 +0300

    fix

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -10,28 +10,28 @@ export class MessageService {
   }
 
   success(message: string) {
-    notify({message: message, width: 300, displayTime: 1000, type: 'success', shading: true}, {
+    notify({message: message, width: 300, displayTime: 1500, type: 'success', shading: true}, {
       position: "top right",
       direction: "up-push"
     });
   }
 
   info(message: string) {
-    notify({message: message, width: 300, displayTime: 1000, type: 'info', shading: true}, {
+    notify({message: message, width: 300, displayTime: 1500, type: 'info', shading: true}, {
       position: "top right",
       direction: "up-push"
     });
   }
 
   error(message: string) {
-    notify({message: message, width: 300, displayTime: 1000, type: 'error', shading: true}, {
+    notify({message: message, width: 300, displayTime: 1500, type: 'error', shading: true}, {
       position: "top right",
       direction: "up-push"
     });
   }
 
   warning(message: string) {
-    notify({message: message, width: 300, displayTime: 1000, type: 'warning', shading: true}, {
+    notify({message: message, width: 300, displayTime: 1500, type: 'warning', shading: true}, {
       position: "top right",
       direction: "up-push"
     });
