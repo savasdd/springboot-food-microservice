81b50f14ee1250ec851f064f7dc9e03793f3fbaf
Savaş Dede <svsdd22@gmail.com>
2023-10-26 17:15:21 +0300

    fix

--- frontend/src/app/views/users/auth-user/auth-user.component.ts
+++ frontend/src/app/views/users/auth-user/auth-user.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {DxDataGridComponent} from "devextreme-angular";
+import {UserService} from "../../../services/user.service";
+import CustomStore from "devextreme/data/custom_store";
 
 @Component({
   selector: 'app-auth-user',
   templateUrl: './auth-user.component.html',
   styleUrls: ['./auth-user.component.scss']
 })
-export class AuthUserComponent {
+export class AuthUserComponent  implements OnInit {
+  dataSource: any = {};
+  @ViewChild('dataSourceGrid', {static: true}) dataSourceGrid: any = DxDataGridComponent;
+  events: Array<string> = [];
 
+  constructor(private service: UserService) {
+    this.loadGrid();
+  }
+
+  ngOnInit(): void {
+  }
+
+  logEvent(eventName: any) {
+    this.events.unshift(eventName);
+  }
+
+  refreshDataGrid(e: any) {
+    this.dataSourceGrid.instance.refresh();
+  }
+
+  loadGrid() {
+    this.dataSource = new CustomStore({
+      key: 'id',
+      load: (loadOptions) => {
+        return this.service.findAllUser(loadOptions).toPromise().then((response: any) => {
+          return {
+            data: response.data,
+            totalCount: response.totalCount
+          };
+        });
+      },
+    });
+  }
 }
