035c50d0013d575bd8bd415a4c57871530945d6b
Savaş Dede <svsdd22@gmail.com>
2023-10-31 11:33:44 +0300

    fix

--- microservice/api-gateway/src/main/java/com/food/GatewayApplication.java
+++ microservice/api-gateway/src/main/java/com/food/GatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ApiServiceApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiServiceApplication.class,args);
+        SpringApplication.run(GatewayApplication.class,args);
     }
 }
