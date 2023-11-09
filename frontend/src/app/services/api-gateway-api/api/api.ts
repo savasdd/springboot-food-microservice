c305665bed43b2b51faf048daeb800020134ce2f
Savaş Dede <svsdd22@gmail.com>
2023-11-09 10:00:11 +0300

    fix bug

--- frontend/src/app/services/api-gateway-api/api/api.ts
+++
@@ -1,7 +0,0 @@
-export * from './userAuthController.service';
-import {UserAuthController} from "./userAuthController.service";
-
-export * from './authController.service';
-import {AuthControllerService} from './authController.service';
-
-export const APIS = [AuthControllerService, UserAuthController];
